71159f00db3c014d092a6e86a05f104d38ef013d
jennjaco3023 <jennjaco3023@gmail.com>
2020-12-14 22:08:18 -0600

    Completed Assignment Requirements

--- TechJobsPersistent/Controllers/HomeController.cs
+++ TechJobsPersistent/Controllers/HomeController.cs
@@ -32,12 +32,38 @@ namespace TechJobsPersistent.Controllers
         [HttpGet("/Add")]
         public IActionResult AddJob()
         {
-            return View();
+            AddJobViewModel addJobViewModel = new AddJobViewModel(context.Employers.ToList(), context.Skills.ToList());
+            return View(addJobViewModel);
         }
 
-        public IActionResult ProcessAddJobForm()
+        public IActionResult ProcessAddJobForm(AddJobViewModel addJobViewModel, string[] selectedSkills)
         {
-            return View();
+
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = addJobViewModel.Name,
+                    EmployerId = addJobViewModel.EmployerId,
+
+                };
+                foreach (string skill in selectedSkills)
+                {
+                    JobSkill newJobSkill = new JobSkill
+                    {
+                        JobId = newJob.Id,
+                        SkillId = int.Parse(skill)
+                    };
+                    context.JobSkills.Add(newJobSkill);
+                };
+
+
+                context.Jobs.Add(newJob);
+                context.SaveChanges();
+                return Redirect("Index");
+            }
+
+            return View("Add", addJobViewModel);
         }
 
         public IActionResult Detail(int id)
---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Job Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        public int EmployerId { get; set; }
+
+        public List<SelectListItem> Employers { get; set; }
+        public List<Skill> Skills { get; set; }
+
+        public AddJobViewModel(List<Employer> possibleEmployers, List<Skill> skills)
+        {
+            Employers = new List<SelectListItem>();
+
+
+            Skills = skills;
+
+            foreach (var employer in possibleEmployers)
+            {
+                Employers.Add(new SelectListItem
+                {
+                    Value = employer.Id.ToString(),
+                    Text = employer.Name
+                });
+            }
+        }
+
+
+        public AddJobViewModel()
+        {
+        }
+    }
+}
