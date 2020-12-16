74fadf592d9d045746c2cefc9b46273ca267c7a6
jennjaco3023 <jennjaco3023@gmail.com>
2020-12-15 20:15:37 -0600

    Submitted SQL Queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,10 @@
 --Part 1
+SELECT * FROM Jobs;
+DESCRIBE Jobs;
 
 --Part 2
+SELECT * FROM Employers WHERE location = "St. Louis";
 
 --Part 3
-
+SELECT Name, Description FROM Skills
+INNER JOIN JobSkills ON JobSkills.SkillId = Skills.Id;
